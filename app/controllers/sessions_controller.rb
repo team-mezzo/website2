e540755f232d80d2ad3bc7f9cf480e0556a9162d
16chuang <16chuang@castilleja.org>
2014-12-31 12:24:41 -0800

    Changed some redirects - go to donations as root if logged in, go to donations once logged in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :redirect_if_logged_in, only: [:new, :create]
+
   def new
   end
 
@@ -8,7 +10,7 @@ class SessionsController < ApplicationController
   		# log user in and redirect to user's donations page
   		log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user) # remember me checkbox
-  		redirect_back_or user # redirect to the page they were trying to access or (default) to their profile
+  		redirect_back_or donations_path # redirect to the page they were trying to access or (default) to their profile
   	else
   		flash.now[:danger] = 'Invalid email/password combination' # flash notice error message
     		render 'new' # try log in again
@@ -19,4 +21,11 @@ class SessionsController < ApplicationController
   	log_out if logged_in? # to prevent log out confusion on multiple browser tabs
   	redirect_to login_path
   end
+
+  private
+    def redirect_if_logged_in
+      if logged_in?
+        redirect_to donations_path
+      end
+    end
 end
